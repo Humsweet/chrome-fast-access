410feade7bd38e3126eb0ff5de26f5a7f52519b5
Humsweet <humsweet811@gmail.com>
2026-01-28 20:15:46 +0900

    feat: add SVG icons to the default speed dial entries and adjust minor whitespace.

--- newtab.js
+++ newtab.js
@@ -165,7 +165,7 @@ class SpeedDial {
 
     // 检查总容量（留 10KB 给 settings 等）
     if (estimatedSize > SYNC_QUOTA_BYTES - 10240) {
-      console.warn(`总数据 (${(estimatedSize/1024).toFixed(1)}KB) 超出同步限制 (90KB)，将仅保存到本地`);
+      console.warn(`总数据 (${(estimatedSize / 1024).toFixed(1)}KB) 超出同步限制 (90KB)，将仅保存到本地`);
       this.showSyncWarning('数据过大，仅保存到本地。请减少快捷方式数量或简化 SVG 图标。');
       return this.saveToLocalOnly({
         dials: this.dials,
@@ -333,7 +333,7 @@ class SpeedDial {
     el.dataset.index = index;
 
     const iconHtml = this.getIconHtml(dial);
-    
+
     el.innerHTML = `
       <div class="dial-icon">${iconHtml}</div>
       <span class="dial-name">${this.escapeHtml(dial.name)}</span>
@@ -1045,10 +1045,10 @@ class SpeedDial {
   // 获取默认快捷方式
   getDefaultDials() {
     return [
-      { name: 'Google', url: 'https://www.google.com', icon: '' },
-      { name: 'YouTube', url: 'https://www.youtube.com', icon: '' },
-      { name: 'GitHub', url: 'https://www.github.com', icon: '' },
-      { name: 'Twitter', url: 'https://www.twitter.com', icon: '' }
+      { name: 'Google', url: 'https://www.google.com', icon: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Google</title><path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"/></svg>' },
+      { name: 'YouTube', url: 'https://www.youtube.com', icon: '<svg fill="#FF0000" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>YouTube</title><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>' },
+      { name: 'GitHub', url: 'https://www.github.com', icon: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>' },
+      { name: 'Twitter', url: 'https://www.twitter.com', icon: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>X</title><path d="M14.234 10.162 22.977 0h-2.072l-7.591 8.824L7.251 0H.258l9.168 13.343L.258 24H2.33l8.016-9.318L16.749 24h6.993zm-2.837 3.299-.929-1.329L3.076 1.56h3.182l5.965 8.532.929 1.329 7.754 11.09h-3.182z"/></svg>' }
     ];
   }
 
